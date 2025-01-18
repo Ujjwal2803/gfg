642f43e9b399db5901901279ffde6e76ff9b947a
Ujjwal-Bharadwaj <114858926+Ujjwal-Bharadwaj@users.noreply.github.com>
2025-01-18 11:22:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        // map<int,int> a;
+        // for(int i=0;i<arr.size();i++){
+        //     int b=arr[i];
+        //     int n=target-b;
+        //     if(a.find(n)!=a.end()){
+        //         return true;
+        //     }
+        //     a[b]=i;
+        // }
+        // return false;
+        set<int> a;
+        for(int i=0;i<arr.size();i++){
+            int b=arr[i];
+            int n=target-b;
+            if(a.find(n)!=a.end()){
+                return true;
+            }
+            a.insert(b);
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.twoSum(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
