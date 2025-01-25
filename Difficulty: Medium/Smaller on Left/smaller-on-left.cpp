83abb97c1d79062ce75d9dad3c8ea530de9575c3
Ujjwal-Bharadwaj <114858926+Ujjwal-Bharadwaj@users.noreply.github.com>
2025-01-25 09:46:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smaller on Left/smaller-on-left.cpp
+++ Difficulty: Medium/Smaller on Left/smaller-on-left.cpp
@@ -1,78 +1,54 @@
 //{ Driver Code Starts
+// Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> Smallestonleft(int arr[], int n);
+// } Driver Code Ends
+// User function Template for C++
 
-int main() {
-
-    int t;
-
-    cin >> t;
-
-    while (t--) {
-
-        int n;
-        cin >> n;
-        int arr[n + 1];
-
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int arr[]){
+        // code here
+        vector<int>p;
+        stack<int>q;
+        for(int i=0;i<=n-1;i++){
+            while(q.size() && q.top()>=arr[i]){// monotonicity
+                q.pop();
+            }
+            if(q.size()==0){
+                p.push_back(-1);
+            }else{
+                p.push_back(q.top());
+            }
+            q.push(arr[i]);
         }
+        
+         return p;
+    }
+};
 
-        vector<int> b;
-        b = Smallestonleft(arr, n);
-        for (int i = 0; i < n; i++)
-            cout << b[i] << " ";
+//{ Driver Code Starts.
 
-        cout << endl;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
     
 cout << "~" << "\n";
 }
-
     return 0;
 }
 // } Driver Code Ends
-
-
-// vector<int> Smallestonleft(int arr[], int n) {
-//     // Complete the function
-//     vector<int>p;
-//         stack<int>q;
-//         for(int i=0;i<=n-1;i++){
-//             while(q.size() && q.top()>arr[i]){// monotonicity
-//                 q.pop();
-//             }
-//             if(q.size()==0){
-//                 p.push_back(-1);
-//             }else{
-//                 p.push_back(q.top());
-//             }
-//             q.push(arr[i]);
-//         }
-        
-//          return p;
-// }
-
-vector<int> Smallestonleft(int arr[], int n) {
-    vector<int> result;
-    set<int> s;
-    for (int i = 0; i < n; ++i) {
-        int current = arr[i];
-        auto it = s.lower_bound(current);
-        if (it == s.begin()) {
-            result.push_back(-1);
-        } else {
-            --it;
-            result.push_back(*it);
-        }
-        s.insert(current);
-    }
-    return result;
-}
-
-
-
-
-
-
