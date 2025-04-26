c9d80f30bbbfc0f71983ce582390dafed2633852
Ujjwal Bharadwaj <114858926+Ujjwal2803@users.noreply.github.com>
2025-04-26 19:55:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -6,45 +6,38 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int countFreq(vector<int>& arr, int k) {
+    int countFreq(vector<int>& arr, int target) {
         // code here
-        int start = -1, end = -1;
-        int low = 0, high = arr.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == k) {
-                start = mid;
-                high = mid - 1;
-            } else if (arr[mid] < k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        int n=arr.size();
+        int start=-1,end=-1;
+        int l=0,h=n-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[mid]==target){
+                start=mid;
+                h=mid-1;
             }
+            else if(arr[mid]<target)l=mid+1;
+            else h=mid-1;
         }
-        // If k is not found, return 0
-    if (start == -1) return 0;
-        low = 0;
-        high = arr.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == k) {
-                end = mid;
-                low = mid + 1;
-            } else if (arr[mid] < k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        l=0,h=n-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[mid]==target){
+                end=mid;
+                l=mid+1;
             }
+            else if(arr[mid]<target)l=mid+1;
+            else h=mid-1;
         }
-        // if (start == -1 && end == -1) {
-        //     return {-1, -1};
-        // }
-
+        if (start == -1 || end == -1) return 0;
         return end-start+1;
     }
 };
+
 
 
 //{ Driver Code Starts.
