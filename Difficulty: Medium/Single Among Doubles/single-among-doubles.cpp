c5ab07f5041df49c02fbd2e854ef986d7e684309
Ujjwal Bharadwaj <114858926+Ujjwal2803@users.noreply.github.com>
2025-06-23 10:29:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Single Among Doubles/single-among-doubles.cpp
@@ -0,0 +1,12 @@
+// User function template for C++
+class Solution {
+  public:
+    int search(int n, int arr[]) {
+        // code
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans^=arr[i];
+        }
+        return ans;
+    }
+};
