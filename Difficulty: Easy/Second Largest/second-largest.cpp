44ed3395fed4ad3e2aad1cea18d4e296eba9d9b7
Ujjwal Bharadwaj <114858926+Ujjwal2803@users.noreply.github.com>
2025-09-10 11:26:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.cpp
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -1,53 +1,19 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int getSecondLargest(vector<int> &arr) {
         // code here
-        int ans=0;
+        int l=0;
         for(int i=0;i<arr.size();i++){
-            if(ans<arr[i]){
-                ans=arr[i];
+            if(arr[i]>l){
+                l=arr[i];
             }
         }
-        int p=-1;
+        int l2=-1;
         for(int i=0;i<arr.size();i++){
-            if(p<arr[i] & arr[i]!=ans){
-                p=arr[i];
+            if(arr[i]>l2 && arr[i]!=l){
+                l2=arr[i];
             }
         }
-        return p;
+        return l2;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        int ans = ob.getSecondLargest(arr);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
