f404e55727631b20c8a179cddc277a610e70a3d5
Ujjwal Bharadwaj <114858926+Ujjwal2803@users.noreply.github.com>
2025-10-26 20:46:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -4,7 +4,7 @@ class Solution {
     int nthFibonacci(int n) {
         // code here
         if(n==0)return 0;
-        if(n==1 || n==2)return 1;
+        if(n==1 )return 1;
         return nthFibonacci(n-1)+nthFibonacci(n-2);
     }
 };
